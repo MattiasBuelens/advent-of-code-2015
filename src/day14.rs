c37a070c59fdbe181b360424cb0e5ea89e2bc9d7
Mattias Buelens <mattias@buelens.com>
2021-12-31 16:21:42 +0100

    Solve day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -37,11 +37,13 @@ pub fn input_generator(input: &str) -> Vec<Reindeer> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
+#[derive(Debug)]
 struct ReindeerState {
     reindeer: Reindeer,
     distance: u32,
     is_resting: bool,
     remaining: u32,
+    points: u32,
 }
 
 impl ReindeerState {
@@ -51,6 +53,7 @@ impl ReindeerState {
             is_resting: false,
             remaining: reindeer.fly_duration,
             reindeer,
+            points: 0,
         }
     }
 
@@ -68,6 +71,10 @@ impl ReindeerState {
             };
         }
     }
+
+    fn award_point(&mut self) {
+        self.points += 1;
+    }
 }
 
 fn simulate(reindeer: &[Reindeer], seconds: usize) -> Vec<ReindeerState> {
@@ -79,14 +86,21 @@ fn simulate(reindeer: &[Reindeer], seconds: usize) -> Vec<ReindeerState> {
         for state in states.iter_mut() {
             state.step();
         }
+        // At the end of each second, he awards one point to the reindeer currently in the lead.
+        // (If there are multiple reindeer tied for the lead, they each get one point.)
+        let leader_distance = states.iter().map(|state| state.distance).max().unwrap();
+        for state in states.iter_mut() {
+            if state.distance == leader_distance {
+                state.award_point();
+            }
+        }
     }
     states
 }
 
 #[aoc(day14, part1)]
 pub fn part1(input: &[Reindeer]) -> u32 {
-    let states = simulate(input, 2503);
-    states
+    simulate(input, 2503)
         .into_iter()
         .map(|state| state.distance)
         .max()
@@ -94,8 +108,12 @@ pub fn part1(input: &[Reindeer]) -> u32 {
 }
 
 #[aoc(day14, part2)]
-pub fn part2(input: &[Reindeer]) -> i32 {
-    todo!()
+pub fn part2(input: &[Reindeer]) -> u32 {
+    simulate(input, 2503)
+        .into_iter()
+        .map(|state| state.points)
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -115,12 +133,24 @@ Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
     #[test]
     fn test_part1() {
         let input = input_generator(&TEST_INPUT);
-        let states = simulate(&input, 1000);
-        let winner = states
-            .into_iter()
-            .max_by_key(|state| state.distance)
-            .unwrap();
+        let mut states = simulate(&input, 1000);
+        states.sort_by_key(|state| state.distance);
+        let [loser, winner]: [ReindeerState; 2] = states.try_into().unwrap();
         assert_eq!(winner.reindeer.name, "Comet");
         assert_eq!(winner.distance, 1120);
+        assert_eq!(loser.reindeer.name, "Dancer");
+        assert_eq!(loser.distance, 1056);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = input_generator(&TEST_INPUT);
+        let mut states = simulate(&input, 1000);
+        states.sort_by_key(|state| state.points);
+        let [loser, winner]: [ReindeerState; 2] = states.try_into().unwrap();
+        assert_eq!(winner.reindeer.name, "Dancer");
+        assert_eq!(winner.points, 689);
+        assert_eq!(loser.reindeer.name, "Comet");
+        assert_eq!(loser.points, 312);
     }
 }
